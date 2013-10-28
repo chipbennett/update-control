2b3bf70c9368c0869e2c8e77b04c163e0c64e067
chipbennett <chip@chipbennett.net>
2013-10-28 11:03:52 -0400

    Version 1.1.1 bugfix

--- update-control.php
+++ update-control.php
@@ -4,7 +4,7 @@
  * Plugin URI: http://github.com/georgestephanis/update-control/
  * Description: Adds a manual toggle to the WordPress Admin Interface for managing auto-updates.
  * Author: George Stephanis
- * Version: 1.1
+ * Version: 1.1.1
  * Author URI: http://stephanis.info/
  */
 
